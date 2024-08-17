52fa17f5eeb31b03627adb7cd156bdb40655f332
VAIBHAV123456 <vaibhavdudhabhate10@gmail.com>
2024-08-17 16:22:03 +0530

    added origin true

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ app.set("view engine",'ejs')
 app.use(express.urlencoded({extended:false}));
 app.use(cors(
     {
-        origin:['http://localhost:3000'],
+        // origin:['http://localhost:3000'],
+        origin:'true',
         credentials:true,
         // credentials: true,
     // Set the allowed HTTP methods
